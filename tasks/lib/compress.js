20e179244258349c7e9897d3fc46a58561ae4192
roadman <roadman@roadman-mba.local>
2014-03-20 11:22:19 +0800

    date can be optional

--- tasks/lib/compress.js
+++ tasks/lib/compress.js
@@ -136,13 +136,8 @@ module.exports = function(grunt) {
       src.forEach(function(srcFile) {
         var internalFileName = (isExpandedPair) ? file.dest : exports.unixifyPath(path.join(file.dest || '', srcFile));
 
-        var properties = { name: internalFileName };
-
-        if (typeof exports.options.fileSettings === 'function') {
-            exports.options.fileSettings(srcFile, properties);
-        }
-
-        archive.file(srcFile, properties, function(err) {
+        var stat = fs.statSync(srcFile);
+        archive.file(srcFile, { name: internalFileName, date: stat.mtime }, function(err) {
           if (err) {
             archive.emit('error', err);
             return;
